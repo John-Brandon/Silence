55a4e59ee3d64cddfdf00dbc00e8bcae589a3440
John Brandon <jbrandon@gmail.com>
2017-12-21 12:35:28 -0800

    user input for seconds to mute

--- silence.sh
+++ silence.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-# Mute the computer for 90 seconds: 
+# Mute the computer (Mac OS): 
 # Avoid listening to commercials on Spotify. 
-# 90 seconds is the length of their commercial breaks.
-# 
+# 90 seconds is the length of common commercial breaks.
+# Original setup to mute for 90 sec, 
+#   but current code takes user input for seconds to mute
+#   e.g. `./silence.sh 90`
 
-TIME_MUTED=90;
+#TIME_MUTED=90;
+TIME_MUTED=$1;  # try taking user input for seconds to mute
 osascript -e 'set volume output muted true';
 sleep $TIME_MUTED;
 osascript -e 'set volume output muted false';
