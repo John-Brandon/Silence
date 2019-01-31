de1c3c776c10802dab74bb2a999199ca694ac0b1
John Brandon <jbrandon@gmail.com>
2019-01-30 20:31:05 -0800

    slight revision to comment description in header

--- silence.sh
+++ silence.sh
@@ -2,9 +2,9 @@
 # Mute the computer (Mac OS): 
 # Avoid listening to commercials on Spotify. 
 # 90 seconds is the length of common commercial breaks.
-# Original setup to mute for 90 sec, 
+# Original setup was to automatically mute for 90s, 
 #   but current code takes user input for seconds to mute
-#   e.g. `./silence.sh 90`
+#   e.g. `./silence.sh 45` would mute for 45s
 
 #TIME_MUTED=90;
 TIME_MUTED=$1;  # try taking user input for seconds to mute
